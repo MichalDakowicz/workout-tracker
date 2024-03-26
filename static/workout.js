347e4a54df15be0ed76f8adcd6526650b8296e17
Michu <michal.dakowicz@infotech.edu.pl>
2024-03-26 10:25:48 +0100

    code cleanup

--- static/workout.js
+++ static/workout.js
@@ -1,93 +1,89 @@
+// Set the current date as the default value for the date input
 document.getElementById("dateInput").valueAsDate = new Date();
 
 function addSetsFields() {
-    var exercise = document.getElementById("exerciseSelect").value;
-    var sets = document.getElementById("setsInput").value;
-    var setsFieldsContainer = document.getElementById("setsFieldsContainer");
-    var setsFields = document.createElement("div");
+    const sets = document.getElementById("setsInput").value;
+    const setsFieldsContainer = document.getElementById("setsFieldsContainer");
+    const setsFields = document.createElement("div");
     setsFields.className = "setsFields";
+
+    // Initialize the innerHTML with the first set
     setsFields.innerHTML =
         'Set 1 <label for="repsInput1">Reps:</label><input type="number" id="repsInput1" min="1" value="1"><label for="weightInput1">Weight:</label><input type="number" id="weightInput1" min="0" value="0">kg';
-    for (var i = 0; i < sets - 1; i++) {
-        a = i + 2;
-        setsFields.innerHTML +=
-            "<br>Set " +
-            a +
-            ' <label for="repsInput' +
-            a +
-            '">Reps:</label><input type="number" id="repsInput' +
-            a +
-            '" min="1" value="1"><label for="weightInput' +
-            a +
-            '">Weight:</label><input type="number" id="weightInput' +
-            a +
-            '" min="0" value="0">kg';
+
+    // Add the remaining sets
+    for (let i = 1; i < sets; i++) {
+        const setNumber = i + 1;
+        setsFields.innerHTML += `<br>Set ${setNumber} <label for="repsInput${setNumber}">Reps:</label><input type="number" id="repsInput${setNumber}" min="1" value="1"><label for="weightInput${setNumber}">Weight:</label><input type="number" id="weightInput${setNumber}" min="0" value="0">kg`;
     }
+
+    // Add the save button
     setsFields.innerHTML +=
         '<br><button onclick="saveRepsWeight()">Save</button>';
-    if (setsFieldsContainer.childElementCount > 0) {
-        setsFieldsContainer.innerHTML = "";
-    }
+
+    // Clear the container and add the new fields
+    setsFieldsContainer.innerHTML = "";
     setsFieldsContainer.appendChild(setsFields);
 }
+
 function saveRepsWeight() {
-    var ListRepWeight = [];
-    var sets = document.getElementById("setsInput").value;
-    for (var i = 1; i <= sets; i++) {
-        var reps = document.getElementById("repsInput" + i).value;
-        var weight = document.getElementById("weightInput" + i).value;
+    const sets = document.getElementById("setsInput").value;
+    const ListRepWeight = [];
+
+    // Collect the reps and weight for each set
+    for (let i = 1; i <= sets; i++) {
+        const reps = document.getElementById(`repsInput${i}`).value;
+        const weight = document.getElementById(`weightInput${i}`).value;
         ListRepWeight.push([reps, weight]);
     }
-    console.log(ListRepWeight);
-    var setsFieldsContainer = document.getElementById("setsFieldsContainer");
+
+    // Clear the sets fields and display the exercise list
+    const setsFieldsContainer = document.getElementById("setsFieldsContainer");
     setsFieldsContainer.innerHTML = "";
-    var exerciseListContainer = document.getElementById("exercise-list");
-    var exercise = document.getElementById("exerciseSelect").value;
-    var exerciseList = document.createElement("div");
+
+    const exerciseListContainer = document.getElementById("exercise-list");
+    const exercise = document.getElementById("exerciseSelect").value;
+    const exerciseList = document.createElement("div");
     exerciseList.className = "exerciseList";
-    List2 = [];
-    for (let index = 0; index < ListRepWeight.length; index++) {
-        List2.push(
-            "\xa0Set " +
-                (index + 1) +
-                ": " +
-                ListRepWeight[index][0] +
-                " reps, " +
-                ListRepWeight[index][1] +
-                "kg"
-        );
-    }
-    exerciseList.innerHTML = exercise + "<br> " + List2.join(", <br>");
+
+    const List2 = ListRepWeight.map(
+        (set, index) => `Set ${index + 1}: ${set[0]} reps, ${set[1]}kg`
+    );
+
+    exerciseList.innerHTML = `${exercise}<br> ${List2.join(", <br>")}`;
     exerciseListContainer.appendChild(exerciseList);
 }
+
 function saveWorkout() {
-    var date = document.getElementById("dateInput").value;
-    var exerciseListContainer = document.getElementById("exercise-list");
-    var exerciseList =
+    const date = document.getElementById("dateInput").value;
+    const exerciseListContainer = document.getElementById("exercise-list");
+    const exerciseList =
         exerciseListContainer.getElementsByClassName("exerciseList");
-    var exercises = [];
-    var name = document.getElementById("workoutName").value;
-    if (name == "") {
+    const exercises = [];
+    let name = document.getElementById("workoutName").value;
+
+    if (name === "") {
         name = "Workout";
     }
+
     for (let index = 0; index < exerciseList.length; index++) {
-        var exerciseText = exerciseList[index].innerText;
-        var exerciseParts = exerciseText.split("\n");
-        var exerciseName = exerciseParts[0];
-        var sets = exerciseParts.slice(1).map((setText) => {
-            var setTextParts = setText.split(", ");
-            var reps = parseInt(setTextParts[0].split(" ")[2]);
-            var weight = parseInt(setTextParts[1].split(" ")[0]);
+        const exerciseText = exerciseList[index].innerText;
+        const exerciseParts = exerciseText.split("\n");
+        const exerciseName = exerciseParts[0];
+        const sets = exerciseParts.slice(1).map((setText) => {
+            const setTextParts = setText.split(", ");
+            const reps = parseInt(setTextParts[0].split(" ")[2]);
+            const weight = parseInt(setTextParts[1].split(" ")[0]);
             return [reps, weight];
         });
         exercises.push({ [exerciseName]: sets });
     }
-    console.log(date);
-    console.log(exercises);
-    var dateInput = document.getElementById("date");
-    var exercisesInput = document.getElementById("exercises");
+
+    const dateInput = document.getElementById("date");
+    const exercisesInput = document.getElementById("exercises");
     dateInput.value = date;
     exercisesInput.value = JSON.stringify({ [name]: exercises });
+
     document
         .getElementById("return-form")
         .getElementsByTagName("input")[2]
