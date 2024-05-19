a5dc1cd2c3a6bc9077a7f7a3cf774466c8a95483
Michu <michal.dakowicz@infotech.edu.pl>
2024-05-19 17:08:02 +0200

    js optimalization

--- static/exercises.js
+++ static/exercises.js
@@ -1,49 +1,16 @@
 function displayExercises() {
     var group = document.getElementById("muscle-group-select").value;
+    var exerciseList = document.getElementById("exercise-lists");
 
-    if (group === "chest") {
-        document.getElementById("chest").style.display = "block";
-        document.getElementById("back").style.display = "none";
-        document.getElementById("shoulders").style.display = "none";
-        document.getElementById("legs").style.display = "none";
-        document.getElementById("arms").style.display = "none";
-        document.getElementById("core").style.display = "none";
-    } else if (group === "back") {
-        document.getElementById("chest").style.display = "none";
-        document.getElementById("back").style.display = "block";
-        document.getElementById("shoulders").style.display = "none";
-        document.getElementById("legs").style.display = "none";
-        document.getElementById("arms").style.display = "none";
-        document.getElementById("core").style.display = "none";
-    } else if (group === "shoulders") {
-        document.getElementById("chest").style.display = "none";
-        document.getElementById("back").style.display = "none";
-        document.getElementById("shoulders").style.display = "block";
-        document.getElementById("legs").style.display = "none";
-        document.getElementById("arms").style.display = "none";
-        document.getElementById("core").style.display = "none";
-    } else if (group === "legs") {
-        document.getElementById("chest").style.display = "none";
-        document.getElementById("back").style.display = "none";
-        document.getElementById("shoulders").style.display = "none";
-        document.getElementById("legs").style.display = "block";
-        document.getElementById("arms").style.display = "none";
-        document.getElementById("core").style.display = "none";
-    } else if (group === "arms") {
-        document.getElementById("chest").style.display = "none";
-        document.getElementById("back").style.display = "none";
-        document.getElementById("shoulders").style.display = "none";
-        document.getElementById("legs").style.display = "none";
-        document.getElementById("arms").style.display = "block";
-        document.getElementById("core").style.display = "none";
-    } else if (group === "abs") {
-        document.getElementById("chest").style.display = "none";
-        document.getElementById("back").style.display = "none";
-        document.getElementById("shoulders").style.display = "none";
-        document.getElementById("legs").style.display = "none";
-        document.getElementById("arms").style.display = "none";
-        document.getElementById("core").style.display = "block";
-    }
+    exerciseList
+        .querySelectorAll("div[class^='exercise-container']")
+        .forEach(function (element) {
+            if (element.id === group) {
+                element.style.display = "block";
+            } else {
+                element.style.display = "none";
+            }
+        });
 }
 
 addEventListener("load", displayExercises);
